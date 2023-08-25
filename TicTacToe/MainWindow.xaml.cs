82fe67f2e569f9b9f58436067deeaacc42e6c997
Ethan Chandler <ethanchandler11@gmail.com>
2023-08-25 17:39:29 +0100

    ClickHandlers for the gamelogic to work in sync work need to be done on GameLogic.
    Next task to allow handlers to interact with code and start/ interact with the game

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,15 +16,52 @@ using System.Windows.Shapes;
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<PlayerEnum, ImageSource> PlayerPNG = new()
+        {
+            {PlayerEnum.X, new BitmapImage(new Uri(@"C:\Users\Ethan\Desktop\WPF app TicTacToe\C#\TicTacToe\Assets\X.png")) },
+            {PlayerEnum.O, new BitmapImage(new Uri(@"C:\Users\Ethan\Desktop\WPF app TicTacToe\C#\TicTacToe\Assets\O.png")) }
+        };
+
+        private readonly Image[] Controls = new Image[9];
+        private readonly GameBoard CurrentGame = new();
         public MainWindow()
         {
             InitializeComponent();
-            GameBoard InstanceOfGameBoard = new GameBoard();
+            SetUpGrid();
+            CurrentGame.MoveMade += MoveMade;
         }
+
+        private void SetUpGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Image Control = new Image();
+                GameGrid.Children.Add(Control);
+                Controls[i] = Control;
+            }
+        }
+
+        private void MoveMade(int i)
+        {
+
+        }
+
+
+        private void GameGrid_Press(object sender,MouseButtonEventArgs e)
+        {
+            double Size = GameGrid.Width / 3;
+            Point MousePosition = e.GetPosition(GameGrid);
+            int row = (int)(MousePosition.Y / Size);
+            int col = (int)(MousePosition.X / Size);
+            //bool v = GameBoard.PossibleMove(4);
+            //Changes to be made in the Logic of the game to work properly.
+        }
+
+
+
+
+
     }
 }
